f8467e7347919cbfb1f026e98faef047bda34ccf
Nguyễn Anh Quân <syrexd42@gmail.com>
2024-10-06 02:16:35 +0700

    NavLink and styling

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,16 @@
 import "./App.css";
 import Header from "./components/Header/Header";
-import { Link } from "react-router-dom";
+import { Outlet, NavLink } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="app-container">
-      <Header />
-      <div>
-        test link
-        <button>
-          <Link to="/users">Go to user page</Link>
-        </button>
-        <button>
-          <Link to="/admin">Go to admin page</Link>
-        </button>
+      <div className="header-container"><Header /></div>
+      <div className="main-container">
+        <div className="sidenav-container"></div>
+        <div className="app-container">
+          <Outlet/>
+        </div>
       </div>
     </div>
   );
